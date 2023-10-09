c654c7589d02c9cdb777d2833ee862452b0b19dc
Henning <henning.holgersen@gmail.com>
2023-10-09 20:47:39 +0200

    Added sync flow run

--- projects/jottings/reschedule/flow.py
+++ projects/jottings/reschedule/flow.py
@@ -3,6 +3,7 @@ from prefect import get_run_logger
 from prefect.client import get_client
 from prefect.server.schemas.schedules import CronSchedule
 import datetime
+from prefect.deployments import run_deployment
 
 
 @task
@@ -45,18 +46,30 @@ async def run_once(flow_name, deployment_name):
         rr = await client.create_flow_run_from_deployment(deployment.id)
 
 
+@task
+def simple_run(full_name):
+    """Trigger a deployment to run once, as a subflow"""
+    flow_run = run_deployment(full_name, scheduled_time=datetime.datetime.utcnow())
+
+
 
 @flow(name="Reschedule")
 def reschedule():
     """Run flow and reschedule"""
     flow_name = "update-unemployment-report"
     deployment_name = "Report"
+    full_name = f"{flow_name}/{deployment_name}"
     
+    # Trigger Deployment to run once, async
     run_once(flow_name, deployment_name)
 
+    # Update schedule for deployment
     new_schedule = CronSchedule(cron="0 9 * * 1-5")
     update_schedule(flow_name, deployment_name, new_schedule)
-    
+
+    # Trigger Deployment to run as subflow
+    simple_run(full_name)
+
 
 if __name__ == '__main__':
     reschedule()
